c15e610be1e6f0d0a3376aa27c11c1bd11d33332
Fahrettin Gungor <fahrettin.gungor@proforto.nl>
2021-03-22 11:49:42 +0100

    feat: Implement getStaticProps

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,16 +1,25 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
-export default function Post() {
-  return <Layout>...</Layout>
-}
-
-export async function getStaticPaths() {
-  // Return a list of possible value for id
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
 }
 
 export async function getStaticProps({ params }) {
-  // Fetch necessary data for the blog post using params.id
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
 
 export async function getStaticPaths() {
